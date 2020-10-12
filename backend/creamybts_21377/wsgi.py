086d72d9b3990bc0039588a7082d37357362c72a
Crowdbotics <team@crowdbotics.com>
2020-10-12 00:16:46 +0000

    Update models

--- backend/creamybts_21377/wsgi.py
+++ backend/creamybts_21377/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'creamybts_21377.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "creamybts_21377.settings")
 
 application = get_wsgi_application()
